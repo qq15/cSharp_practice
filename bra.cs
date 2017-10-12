efac117e05cb566c6204dc6a4ffc9dabdfcd32ab
qq15 <30972191+qq15@users.noreply.github.com>
2017-10-12 20:39:45 +0800

    Add files via upload

---
+++ bra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+	class bra
+	{
+		static void Main(string[] args)
+		{
+			int A = 1;int B = 2;int C = 3;
+			int D = 4;int E = 5;int F = 6;
+			int cup = C;
+			if (cup >= D)
+				Console.WriteLine("菇凉请留下电话，谢谢。");
+			
+			else
+				Console.WriteLine("对不起，当我知道你还没D罩杯的时候，我们就已经无缘了。");
+		}
+	}
+}
